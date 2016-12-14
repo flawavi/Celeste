90b889404d8e0e36d00d6cdcc8e6a4ecb8d284c0
Fletcher Watson <fbwthe6@gmail.com>
2016-12-14 17:11:11 -0600

    added getJournies function, ng-repeat to view all possible journies.

--- src/app/controllers/JourneyCtrl.js
+++ src/app/controllers/JourneyCtrl.js
@@ -2,6 +2,23 @@
 
 app.controller('JourneyCtrl', function($scope, $location, JourneyFactory, AuthFactory, ExplorerFactory){
 
-  let explorerId = AuthFactory.getUser().uid
-  $scope.explorer = ExplorerFactory.getExplorerById(explorerId)
-})
+
+  // $scope.listJournies = () => {
+    JourneyFactory.getJournies()
+    .then(data => {
+      console.log(data)
+      $scope.journies = data
+    })
+  // }
+
+  // $scope.explorer = ExplorerFactory.getExplorerById(explorerId)
+  AuthFactory.currentUser().then(user => {
+    console.log(user.uid)
+      ExplorerFactory.getExplorerById(user.uid)
+      .then(explorer => {
+        console.log(explorer)
+        $scope.isLoggedIn = true
+        $scope.explorerName = explorer.firstName
+      })
+    })
+  })
